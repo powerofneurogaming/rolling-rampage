bf78d9e6230a867a2b2dfdbb4a104c1750c102c2
RADLab UCSD <radlab@ucsd.edu>
2019-09-24 00:43:06 +0000

    Level 7 Created!

---
+++ Assets/Flipper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject flipperObject = GameObject.FindWithTag("flipper");
+        Transform flipper = GameObject.FindWithTag("flipper").transform;
+        Flipper2 flipperScript = GameObject.FindWithTag("flipper").GetComponent<Flipper2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void onTriggerEnter(GameObject flipperObject)
+    {
+        Vector3 targetDir = flipperObject.transform.position - transform.position;
+        Flipper2.Rotate(targetDir, 0, 0, 90, Space.Self);
+    }
+}
